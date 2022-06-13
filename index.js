f9ec5b03ebc3f7bc2e0c6aa92db641a49de27cd8
André Díaz <andre290800@gmail.com>
2022-06-13 18:44:20 -0500

    Logical Query Operators

--- index.js
+++ index.js
@@ -28,19 +28,14 @@ async function createCourse() {
 }
 
 async function getCourses() {
-  // eq (equal)
-  // ne (not equal)
-  // gt (greater than)
-  // gte (greater than or equal)
-  // lt (less than)
-  // lte (less than or equal)
-  // in
-  // nin (not in)
+  // or
+  // and
 
   const courses = await Course
     // .find({ author: "Mosh", isPublished: true })
-    // .find({ price: { $gte: 10, $lte: 20 } })
-    .find({ price: { $in: [10, 15, 20] } })
+    .find()
+    .or([{ author: "Mosh" }, { isPublished: true }])
+    .and([])
     .limit(10)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 });
