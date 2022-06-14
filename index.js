433c25ce1f633759b779b7a0fcf83e69a4ecf464
André Díaz <andre290800@gmail.com>
2022-06-14 11:40:59 -0500

    Typo

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const courseSchema = new mongoose.Schema({
   category: {
     type: String,
     required: true,
-    enum: ["web, mobile", "network"],
+    enum: ["web", "mobile", "network"],
   },
   author: String,
   tags: [String],
