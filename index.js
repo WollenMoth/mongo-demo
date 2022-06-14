fb98360e7142eb6e65ce10ac2f02dd09ea59ce85
André Díaz <andre290800@gmail.com>
2022-06-14 11:42:20 -0500

    Custom Validators

--- index.js
+++ index.js
@@ -19,7 +19,15 @@ const courseSchema = new mongoose.Schema({
     enum: ["web", "mobile", "network"],
   },
   author: String,
-  tags: [String],
+  tags: {
+    type: Array,
+    validate: {
+      validator: function (v) {
+        return v && v.length > 0;
+      },
+      message: "A course should have at least one tag.",
+    },
+  },
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
   price: {
@@ -38,9 +46,9 @@ async function createCourse() {
   try {
     const course = new Course({
       name: "Angular Course",
-      category: "-",
+      category: "web",
       author: "Mosh",
-      tags: ["angular", "frontend"],
+      tags: null,
       isPublished: true,
       price: 15,
     });
